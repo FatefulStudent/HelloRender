575d1a15f3c80248de0670f70e27a310a71a73c8
Vlad Ivanov <vladislavladislavovich@gmail.com>
2021-12-10 21:56:17 +0300

    Added clang format from UE4

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clang-format -verbose -i src/*
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make -s
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -15,51 +14,50 @@ void error_callback(int error, const char* description)
     fprintf(stderr, "Error: %s\n", description);
 }
 
-int main( void )
+int main(void)
 {
-	GLFWwindow* window;
-	if( !glfwInit() )
-	{
-		std::cerr << "Failed to initialize GLFW." << std::endl;
-		return -1;
-	}
+    GLFWwindow* window;
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW." << std::endl;
+        return -1;
+    }
 
-	glfwWindowHint(GLFW_SAMPLES, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	glfwSetErrorCallback(error_callback);
+    glfwSetErrorCallback(error_callback);
 
-	window = glfwCreateWindow( 500, 500, "Hello World!", NULL, NULL);
-	if( window == NULL )
-	{
-		std::cerr << "Failed to init GLFW window." << std::endl;
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    window = glfwCreateWindow(500, 500, "Hello World!", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cerr << "Failed to init GLFW window." << std::endl;
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	glewExperimental = true;
-	if (glewInit() != GLEW_OK) 
-	{
-		std::cerr << "Failed to init GLEW." << std::endl;
-		glfwTerminate();
-		return -1;
-	}
+    glewExperimental = true;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "Failed to init GLEW." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
 
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-	do
-	{
-		glClear(GL_COLOR_BUFFER_BIT);
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    do
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
 
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && glfwWindowShouldClose(window) == 0 );
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+             glfwWindowShouldClose(window) == 0);
 
-	glfwTerminate();
-	return 0;
+    glfwTerminate();
+    return 0;
 }
-
