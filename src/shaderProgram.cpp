72209d1ec04cc985d0ae1fecfece0dbef7380387
Vlad Ivanov <vladislavladislavovich@gmail.com>
2021-12-23 00:20:43 +0300

    WIP refactor

---
+++ src/shaderProgram.cpp
@@ -0,0 +1,10 @@
+#include "shaderProgram.h"
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+ShaderProgram::ShaderProgram(const char* vertexPath, const char* fragmentPath)
+{
+    m_id = 1;
+}
