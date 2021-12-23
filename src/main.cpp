b4f1fa758fe13db14e9a743f5812f4969f3db78c
Vlad Ivanov <vladislavvladislavovich@gmail.com>
2021-12-23 18:36:52 +0300

    moved some util functions to helper

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+#include "shaderInstance.h"
+#include "shaderProgram.h"
+#include "Helper/ShaderHelper.h"
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -6,9 +10,6 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
-
-#include "shaderInstance.h"
-#include "shaderProgram.h"
 
 struct Vector3 {
     float x = 0.0f;
@@ -27,53 +28,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 
 void error_callback(int error, const char* description) {
     fprintf(stderr, "Error: %s\n", description);
-}
-
-ShaderInstance* CreateVertexShader() {
-    const char* vertexShaderSource =
-        "#version 420 core\n"
-        "out vec3 Position;"
-        "out vec3 Color;"
-        "layout (location = 0) in vec3 aPosition;\n"
-        "layout (location = 1) in vec3 aColor;\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = vec4(aPosition, 1.0);\n"
-        "   Position = aPosition;"
-        "   Color = aColor;"
-        "}\0";
-
-    return new ShaderInstance(EShaderType::Vertex, vertexShaderSource);
-}
-
-ShaderInstance* CreateFragmentShader1() {
-    const char* fragmentShader1Source =
-        "#version 420 core\n"
-        "in vec3 Position;"
-        "in vec3 Color;"
-        "out vec4 FragColor;\n"
-        "void main()\n"
-        "{\n"
-        "    FragColor = vec4(Color, 1.0f);\n"
-        "}\0;";
-
-    return new ShaderInstance(EShaderType::Fragment,
-                              fragmentShader1Source);
-}
-
-ShaderInstance* CreateFragmentShader2() {
-    const char* fragmentShader2Source =
-        "#version 420 core\n"
-        "in vec3 Position;"
-        "uniform float BlueColor;"
-        "out vec4 FragColor;\n"
-        "void main()\n"
-        "{\n"
-        "    FragColor = vec4(-Position.xy, BlueColor, 1.0f);\n"
-        "}\0;";
-
-    return new ShaderInstance(EShaderType::Fragment,
-                              fragmentShader2Source);
 }
 
 GLFWwindow* CreateWindow() {
@@ -124,22 +78,8 @@ int main(void) {
         return -1;
     }
 
-    ShaderProgram* shaderProgram1;
-    {
-        ShaderInstance* vertexShaderInstance = CreateVertexShader();
-        ShaderInstance* fragmentShaderInstance1 = CreateFragmentShader1();
-        shaderProgram1 = new ShaderProgram(
-            vertexShaderInstance, fragmentShaderInstance1);
-    }
-
-    ShaderProgram* shaderProgram2;
-    {
-        ShaderInstance* vertexShaderInstance = CreateVertexShader();
-        ShaderInstance* fragmentShaderInstance2 = CreateFragmentShader2();
-
-        shaderProgram2 = new ShaderProgram(
-            vertexShaderInstance, fragmentShaderInstance2);
-    }
+    ShaderProgram* shaderProgram1 = ShaderHelper::CreateShaderProgram1();
+    ShaderProgram* shaderProgram2 = ShaderHelper::CreateShaderProgram2();
 
     std::vector<VertexData> Vertices1 = {
         // location          colour
@@ -218,7 +158,7 @@ int main(void) {
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    
+
     delete shaderProgram1;
     delete shaderProgram2;
 
