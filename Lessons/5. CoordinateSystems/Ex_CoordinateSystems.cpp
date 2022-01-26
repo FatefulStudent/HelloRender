66c0e39f9400aa25263ab1f6c288eb6faf10cbf7
Vlad Ivanov <vladislavvladislavovich@gmail.com>
2022-01-26 21:16:52 +0300

    added more cubes!

--- Lessons/5. CoordinateSystems/Ex_CoordinateSystems.cpp
+++ Lessons/5. CoordinateSystems/Ex_CoordinateSystems.cpp
@@ -96,26 +96,44 @@ void Ex_CoordinateSystems::Initialize(GLFWwindow* window) {
 void Ex_CoordinateSystems::Tick() {
     BaseExcercise::Tick();
 
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f),
-                        glm::vec3(0.5f, 1.0f, 0.0f));
-
     glm::mat4 view = glm::mat4(1.0f);
     // note that we're translating the scene in the reverse direction of where
     // we want to move
     view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
-    glm::mat4 projection = glm::mat4(1.0f);
-    projection =
-        glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+    std::vector<glm::vec3> cubePositions = {
+        {0.0f, 0.0f, 0.0f},     {2.0f, 5.0f, -15.0f}, {-1.5f, -2.2f, -2.5f},
+        {-3.8f, -2.0f, -12.3f}, {2.4f, -0.4f, -3.5f}, {-1.7f, 3.0f, -7.5f},
+        {1.3f, -2.0f, -2.5f},   {1.5f, 2.0f, -2.5f},  {1.5f, 0.2f, -1.5f},
+        {-1.3f, 1.0f, -1.5f},
+    };
 
-    m_shaderProgram->use();
-    m_shaderProgram->setMatrix("Model", model);
-    m_shaderProgram->setMatrix("View", view);
-    m_shaderProgram->setMatrix("Projection", projection);
+    for (auto&& cubePosition : cubePositions) {
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, cubePosition);
 
-    m_texture1->Bind();
-    m_texture2->Bind();
+        const bool bHasPositivexAxis = cubePosition.x > 0;
 
-    m_mesh->Draw();
+        model = glm::rotate(model,
+                            (float)glfwGetTime() * glm::radians(50.0f) *
+                                glm::length(cubePosition),
+                            bHasPositivexAxis ? glm::vec3(1.0f, 0.f, 0.0f)
+                                              : glm::vec3(-1.0f, 0.f, 0.0f));
+
+        glm::mat4 projection = glm::mat4(1.0f);
+        projection = bHasPositivexAxis
+                         ? glm::perspective(glm::radians(45.0f),
+                                            800.0f / 600.0f, 0.1f, 100.0f)
+                         : glm::ortho(-4.0f, 4.0f, -3.0f, 3.0f, 0.1f, 1000.f);
+
+        m_shaderProgram->use();
+        m_shaderProgram->setMatrix("Model", model);
+        m_shaderProgram->setMatrix("View", view);
+        m_shaderProgram->setMatrix("Projection", projection);
+
+        m_texture1->Bind();
+        m_texture2->Bind();
+
+        m_mesh->Draw();
+    }
 }
